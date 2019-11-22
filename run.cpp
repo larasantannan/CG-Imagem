d7eaa75d7a62cfef50fdab4d12bbf728539dab54
larasantannan <lara.santannan@gmail.com>
2019-11-22 20:33:57 -0300

    FUNCIONA

--- run.cpp
+++ run.cpp
@@ -1,69 +1,150 @@
-// OpenGL Utility Toolkit
+//This program loads 3-channel BMP
 #include <GL/glut.h>
-#include <fstream>
-#include <iostream>
+#include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <load_bmp.h>
-using namespace std;
 
-// Inicializacoes de OpenGL que devem ser
-// executadas antes da exibicao do desenho
-void Inicializa(){
+// bitmap header structure, has to be packed to avoid compiler padding
+#pragma pack(1)
+typedef struct BITMAPFILEHEADER {
+  char magic[2];       // "BM" 0x424d - char[2] to avoid endian problems
+  uint32_t filesize;   // size of the bitmap file (data + headers)
+  uint16_t reserved1;  // must be 0
+  uint16_t reserved2;  // must be 0
+  uint32_t dataoffset; // when does the data start
+} BITMAPFILEHEADER;
 
-  // Define a janela de visualiza��o
-  glMatrixMode(GLUT_SINGLE | GLUT_RGB);
+typedef struct BITMAPINFOHEADER {
+  uint32_t headersize;      // size of this header
+  int32_t width;            // width of the bmp
+  int32_t height;           // height of the bmp
+  uint16_t colorplanes;     // must be 1
+  uint16_t bitdepth;        // bits per pixel
+  uint32_t compression;     // 0 = uncompressed
+  uint32_t imagesize;       // can be 0 if bmp is uncompressed
+  int32_t hresolution;      // print resolution
+  int32_t vresolution;      // print resolution
+  uint32_t palettecolors;   // can be 0 if uncompressed
+  uint32_t importantcolors; // number of important colors, 0 = all are important
+} BITMAPINFOHEADER;
 
-  // Define o sistema de coordenadas
-  glOrtho(-100.0, 100.0, -100.0, 100.0, -100.0, 100.0);
+typedef struct BITMAPFULLHEADER {
+  BITMAPFILEHEADER fileinfo;
+  BITMAPINFOHEADER bmpinfo;
+} BITMAPFULLHEADER;
+#pragma pack(0)
 
-  // Define a cor de fundo da janela
-  glClearColor(0.0, 0.0, 0.0, 0.0);
+// BMP HEADER
+BITMAPFULLHEADER header;
 
-  glLoadIdentity();
+// image data
+unsigned char *data;        // loaded image
+
+// dynamic programming to improve performance
+
+int loadBMP(const char *imagepath) {
+  // Open the file
+  FILE *file = fopen(imagepath, "rb");
+  if (!file) {
+    printf("Image could not be opened\n");
+    return 0;
+  }
+
+  // Read header
+  if (fread(&header, 1, sizeof(BITMAPFULLHEADER), file) !=
+      54) { // If not 54 bytes read : problem
+    printf("Not a correct BMP file - Wrong header size\n");
+    return 1;
+  }
+
+  if (header.fileinfo.magic[0] != 'B' || header.fileinfo.magic[1] != 'M') {
+    printf("Not a correct BMP file - Wrong magic number\n");
+    return 1;
+  }
+
+  // Read ints from the header
+  unsigned long dataPos = header.fileinfo.dataoffset;
+  unsigned long imageSize = header.bmpinfo.imagesize;
+  long width = header.bmpinfo.width;
+  long height = header.bmpinfo.height;
+
+  // Some BMP files are misformatted, guess missing information
+  if (imageSize == 0) {
+    imageSize = width * height * 3;
+  }
+  if (dataPos == 0) {
+    dataPos = 54; // The BMP header is usually done that way
+  }
+
+  // Allocate buffer
+  data = (unsigned char *)malloc(imageSize);
+
+  // Read the actual data from the file into the buffer
+  fseek(file, dataPos, SEEK_SET);
+  if (!fread(data, 1, imageSize, file)) {
+    printf("Couldn't read BMP Data. Giving up.\n");
+    return 1;
+  }
+
+  // copying components BGR to RGB
+  unsigned char B,R;
+  for(int i = 0; i < width * height ; i++){
+   int index = i*3;
+   B = data[index];
+   R = data[index+2];
+   data[index] = R;
+   data[index+2] = B;
+  }
+
+  fclose(file);
+  return 0;
 }
 
-// Fun��o callback chamada para fazer o desenho
-void Desenha(){
 
-  // Limpa a janela de visualiza��o com a cor
-  // de fundo especificada
+void init(void) { 
+  glClearColor(0.0, 0.0, 0.0, 0.0); 
+}
+
+void display(void) {
+  long width = header.bmpinfo.width;
+  long height = header.bmpinfo.height;
+
   glClear(GL_COLOR_BUFFER_BIT);
-
-  // Define a cor de desenho como vermelho
-  glColor3f(1.0, 0.0, 0.0);
-
-  // Desenha um tri�ngulo
-  glBegin(GL_TRIANGLES);
-    glVertex3f(0.5, 0.5, 0.0);
-    glVertex3f(-0.5, 0.5, 0.0);
-    glVertex3f(0.0, -0.5, 0.0);
-  glEnd();
-
-  // Executa os comandos OpenGL para renderiza��o
+  glRasterPos2i(0, 0);
+  glDrawPixels(width, height, GL_RGB, GL_UNSIGNED_BYTE, data);
+  glRasterPos2i(width, 0);
+  glDrawPixels(width, height, GL_RGB, GL_UNSIGNED_BYTE, data);
   glFlush();
 }
 
-// Programa principal
-int main(int argc, char** argv){
+void reshape(int w, int h) {
+  glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0, w, 0, h, -1.0, 1.0);
+  glMatrixMode(GL_MODELVIEW);
+}
 
-  // Inicia GLUT e processa argumentos passados por linha de comandos
+void keyboard(unsigned char key, int x, int y) {
+  switch (key) {
+  case 27:
+    exit(0);
+  }
+}
+
+int main(int argc, char **argv) {
+  loadBMP(argv[1]);
+  long width = header.bmpinfo.width;
+  long height = header.bmpinfo.height;
   glutInit(&argc, argv);
-
-  // Avisa a GLUT que tipo de modo de exibi��o deve ser usado quando a janela � criada
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-
-  // Cria uma janela GLUT que permite a execu��o de comandos OpenGL
-  glutCreateWindow("Ol� Mundo!");
-
-  // Define a fun��o respons�vel por redesenhar a janela OpenGL sempre que necess�rio
-  glutDisplayFunc(Desenha);
-
-  // Inicializa��es de OpenGL executadas antes da exibi��o do desenho
-  Inicializa();
-
-  // Inicia o processamento de eventos de GLUT. O controle do programa
-  // passa a GLUT, que inicia o gerenciamento dos eventos
+  glutInitWindowSize(width * 2, height);
+  glutInitWindowPosition(100, 100);
+  glutCreateWindow(argv[0]);
+  init();
+  glutReshapeFunc(reshape);
+  glutKeyboardFunc(keyboard);
+  glutDisplayFunc(display);
   glutMainLoop();
-
   return 0;
 }
